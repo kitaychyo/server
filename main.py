1c0e5383ac2406160ad1f6a2bd36e288825387da
kitaychyo <ooo85861@gmail.com>
2025-04-25 16:42:06 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+# Пример данных для отправки
+sample_data = {
+    "device_id": "device_001",
+    "status": "active",
+    "value": 42
+}
+
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    # Синхронная обработка запроса
+    return jsonify(sample_data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
